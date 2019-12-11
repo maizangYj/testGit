ee141072d17beb9eb7b692164ce236962a15837b
xyz <2582672760@qq.com>
2019-12-11 23:37:12 +0800

    init

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * @author : pengliang
+ * @version V1.0
+ * @Project: studyGit
+ * @Package PACKAGE_NAME
+ * @Descriptio /**
+ * @Project: studyGit
+ * @Package PACKAGE_NAME
+ * @Description: TODO
+ * @date Date : 2019年12月11日 23:35
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("testGit");
+    }
+}
